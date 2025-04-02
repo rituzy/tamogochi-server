b915f44018006d013fa966595198c38d2f56a366
Roman Izutov <roman.izutov@gmail.com>
2025-04-02 22:54:58 +0300

    vercel_publishing_error

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,14 @@ import { createServer } from './serverInit'
 const handler: any = async (req: VercelRequest, resp: VercelResponse) => {
     try{
         const hapiServer: any = await createServer();
+        const hapiResponse: any = await hapiServer.inject({
+            method: req.method as any,
+            url: req.url!,
+            headers: req.headers,
+            payload: req.body
+        });
 
+        resp.status(hapiResponse.statusCode).send(hapiResponse.payload);
     } catch (error) {
         console.error('Ошибка при обработке запроса:' , error);
         resp.status(500).json({
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { createServer } from './serverInit'
 
-const init = async () => {
+export const init = async () => {
   const server = await createServer();
   await server.start();
   console.log('Локальный сервер запущен на %s', server.info.uri);
